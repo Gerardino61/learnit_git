2f347d3e24fc51f36646b60ba175e6e06e34b39e
Gerardino <gerardlabaki@gmail.com>
2021-09-02 22:49:10 +0300

    test SSH connection

--- src/HelloPrinter.java
+++ src/HelloPrinter.java
@@ -1,10 +1,6 @@
-
 public class HelloPrinter {
 
-	public static void main(String[] args) {                      
-	
+	public static void main(String[] args) {                          	
 		System.out.println("Hello World!");
-
 	}
-
 }
