7e052795b56ea29e342ba259c668ad45c5795111
Gerardino <gerardlabaki@gmail.com>
2021-09-02 17:33:50 +0300

    vi editor test - Added .gitignore and removed bin/ from staging

--- src/HelloPrinter.java
+++ src/HelloPrinter.java
@@ -1,7 +1,7 @@
 
 public class HelloPrinter {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) {                      
 	
 		System.out.println("Hello World!");
 
